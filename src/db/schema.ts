a258b5ddb3cf7a54cdd15e16491dcc2fc491b237
hansfigo <hambasahaya0303@gmail.com>
2025-03-28 20:54:46 +0700

    feat: Add user schema fields for gender, birth date, phone, and university

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,7 @@
+import { relations } from "drizzle-orm";
 import {
   bigint,
+  date,
   decimal,
   int,
   mysqlEnum,
@@ -8,7 +10,6 @@ import {
   timestamp,
   varchar,
 } from "drizzle-orm/mysql-core";
-import { relations } from "drizzle-orm";
 import { send } from "process";
 
 const transactionCategoryEnums = mysqlEnum("category", [
@@ -28,17 +29,26 @@ const riskManagementEnums = mysqlEnum("risk_management", [
   "high",
 ]);
 
+const genderEnums = mysqlEnum("gender", ["male", "female", "other"]);
+
 // user table schema
 export const usersTable = mysqlTable("users", {
   id: bigint({ mode: "number" }).primaryKey().autoincrement(),
   role: int().notNull().default(0).notNull(), // 0 for user, 1 for admin
   full_name: varchar({ length: 255 }).notNull(),
   age: int().notNull().default(17),
+  phone: varchar({ length: 15 }).notNull(), //
   balance: decimal({ precision: 16, scale: 2 }).notNull().default("0.0"),
   username: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
   profilePicture: varchar("profile_picture", { length: 256 }),
+
+  // Onboarding
+  gender: genderEnums.default("other"),
+  birth_date: date(),
+  university: varchar({ length: 255 }), // Optional
+
   createdAt: timestamp("created_at"),
   updatedAt: timestamp("updated_at"),
 });
@@ -83,16 +93,16 @@ export const itemsTable = mysqlTable("transaction_items", {
   updatedAt: timestamp("updated_at"),
 });
 
-export const transactionRelations = relations(transactionsTable, ({ one }) => ({
-  items: one(itemsTable, {
-    fields: [transactionsTable.id],
-    references: [itemsTable.transaction_id],
-  }),
-}));
+// export const transactionRelations = relations(transactionsTable, ({ one }) => ({
+//   items: one(itemsTable, {
+//     fields: [transactionsTable.id],
+//     references: [itemsTable.transaction_id],
+//   }),
+// }));
 
-export const itemsRelation = relations(itemsTable, ({ one }) => ({
-  transactionsTable: one(transactionsTable, {
-    fields: [itemsTable.transaction_id],
-    references: [transactionsTable.id],
-  }),
-}));
+// export const itemsRelation = relations(itemsTable, ({ one }) => ({
+//   transactionsTable: one(transactionsTable, {
+//     fields: [itemsTable.transaction_id],
+//     references: [transactionsTable.id],
+//   }),
+// }));
