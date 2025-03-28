5a0b70836c1857546cb64287747c8b19bd6f4a4d
hansfigo <hambasahaya0303@gmail.com>
2025-03-28 23:00:07 +0700

    migrate

--- drizzle/0021_easy_thor.sql
+++ drizzle/0021_easy_thor.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `users` ADD CONSTRAINT `users_phone_unique` UNIQUE(`phone`);
+-- ALTER TABLE `users` ADD CONSTRAINT `users_phone_unique` UNIQUE(`phone`);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -37,7 +37,7 @@ export const usersTable = mysqlTable("users", {
   role: int().notNull().default(0).notNull(), // 0 for user, 1 for admin
   full_name: varchar({ length: 255 }).notNull(),
   age: int(),
-  phone: varchar({ length: 15 }).notNull().unique(), //
+  phone: varchar({ length: 15 }).unique(), //
   balance: decimal({ precision: 16, scale: 2 }).notNull().default("0.0"),
   username: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
