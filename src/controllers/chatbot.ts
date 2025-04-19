0241359aedbef8c41b225075f83aa244e4514ad3
masdewaa <ramasyailana3@gmail.com>
2025-04-19 18:46:56 +0700

    Update chatbot.ts

--- src/controllers/chatbot.ts
+++ src/controllers/chatbot.ts
@@ -76,7 +76,7 @@ const getChatbotResponse = async (userId: number, userMessage: string) => {
     messages: [
       {
         role: "system",
-        content: "You are a financial assistant helping users manage money wisely. make sure to provide accurate and helpful advice in one paragraf.",
+        content: "You are a financial assistant helping users manage money wisely. make sure to provide accurate and helpful advice, simple and in one paragraf.",
       },
       {
         role: "user",
